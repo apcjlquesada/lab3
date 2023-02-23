09a7ba07736519804fd95f4f9af35b09cfa5399d
Joe Gene Quesada <jlquesada@apc.edu.ph>
2023-02-23 13:07:06 +0800

    Code Igniter 4 Create News Item Tutorial

--- ci4/app/Models/NewsModel.php
+++ ci4/app/Models/NewsModel.php
@@ -8,7 +8,9 @@ class NewsModel extends Model
 {
     protected $table = 'news';
 	
-    public function getNews($slug = false)
+	protected $allowedFields = ['title', 'slug', 'body'];
+	
+	public function getNews($slug = false)
     {
         if ($slug === false) {
             return $this->findAll();
@@ -16,8 +18,6 @@ class NewsModel extends Model
 
         return $this->where(['slug' => $slug])->first();
     }
-
-	
 	
 	
 }
