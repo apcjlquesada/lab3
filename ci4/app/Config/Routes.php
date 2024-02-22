a69ce9a34fe0f72e189a74659981645e7ed3d542
Joe Gene Q <jlquesada@apc.edu.phclear>
2024-02-22 16:49:45 +0800

    Copied News to Guests and edited files

--- ci4/app/Config/Routes.php
+++ ci4/app/Config/Routes.php
@@ -7,8 +7,16 @@ use CodeIgniter\Router\RouteCollection;
  */
 $routes->get('/', 'Home::index');
 
-use App\Controllers\News; 
+use App\Controllers\News;
+use App\Controllers\Guests;  
 use App\Controllers\Pages;
+
+$routes->get('guests', [Guests::class, 'index']);           
+$routes->get('guests/new', [Guests::class, 'new']); // Add this line
+$routes->post('guests', [Guests::class, 'create']); // Add this line
+$routes->get('guests/(:segment)', [Guests::class, 'show']); 
+
+
 
 $routes->get('news', [News::class, 'index']);           
 $routes->get('news/new', [News::class, 'new']); // Add this line
