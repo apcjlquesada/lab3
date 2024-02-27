907b13833c34ce31e7bf666b41bc17957994a9d3
Joe Gene Q <jlquesada@apc.edu.phclear>
2024-02-27 08:54:38 +0800

    Revert "Copied News to Guests and edited files" Yes
    This reverts commit a69ce9a34fe0f72e189a74659981645e7ed3d542.

--- ci4/app/Config/Filters.php
+++ ci4/app/Config/Filters.php
@@ -56,7 +56,7 @@ class Filters extends BaseConfig
      * permits any HTTP method to access a controller. Accessing the controller
      * with a method you don't expect could bypass the filter.
      */
-    public array $methods = ['csrf'];
+    public array $methods = [];
 
     /**
      * List of filter aliases that should run on any
